97289fd67033b84007e6b8d01b193a79538873a3
Zyzus <45143612+Zyzus@users.noreply.github.com>
2019-12-22 22:20:28 +0100

    full

---
+++ barka_extra.ino
@@ -0,0 +1,247 @@
+/*
+ Darude - Sandstorm
+ 
+ (c) Jordi Agricola 2014
+ 
+ Speaker on port 8
+ 
+ */
+ #include "pitches.h"
+ 
+// notes in the melody:
+ 
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+ 
+ 
+ int n1 = 300;
+ int n2 = 600;
+ int n3 = 900;
+ int n4 = 1100;
+ int n5 = 1400;
+ int n6 = 1500;
+ int n7 = 1700;
+
+
+ int standardDelay = 50;
+ int delayBetweenBars = 50;
+ 
+void setup() {
+
+
+  tone(8,  NOTE_E5, n7);
+ delay(n7);
+
+  tone(8,  NOTE_D5, n1);
+ delay(n1);
+ 
+  tone(8,  NOTE_E5, n1);
+ delay(n1);
+ 
+  tone(8,  NOTE_F5, n1);
+ delay(n1);
+
+  tone(8,  NOTE_E5, n1);
+ delay(n1);
+
+   tone(8,  NOTE_D5, n1);
+ delay(n1);
+ 
+  tone(8,  NOTE_C5, n3);
+ delay(n3);
+ 
+ tone(8,  NOTE_C5, n6);
+ delay(n6);
+
+  tone(8,  NOTE_D5, n2);
+ delay(n2);
+ 
+  tone(8,  NOTE_E5, n1);
+ delay(n1);
+ 
+ tone(8,  NOTE_F5, n3);
+ delay(n3);
+ 
+  tone(8,  NOTE_F5, n6);
+ delay(n6);
+
+ tone(8,  NOTE_F5, n1);
+ delay(n1);
+
+ tone(8,  NOTE_F5, n1);
+ delay(n1);
+
+ tone(8,  NOTE_F5, n1);
+ delay(n1);
+
+ tone(8,  NOTE_E5, n1);
+ delay(n1);
+
+  tone(8,  NOTE_D5, n3);
+ delay(n3);
+
+ tone(8,  NOTE_D5, n6);
+ delay(n6);
+
+ tone(8,  NOTE_G5, n1);
+ delay(n1);
+
+ tone(8,  NOTE_C5, n2);
+ delay(n2);
+
+ tone(8,  NOTE_D5, n1);
+ delay(n1);
+
+ tone(8,  NOTE_E5, n3);
+ delay(n3);
+
+ tone(8,  NOTE_E5, n6);
+ delay(n6);
+
+ tone(8,  NOTE_E5, n1);
+ delay(n1);
+
+  tone(8,  NOTE_F5, n2);
+ delay(n2);
+
+  tone(8,  NOTE_D5, n1);
+ delay(n1);
+
+ tone(8,  NOTE_C5, n3);
+ delay(n3);
+
+ tone(8,  NOTE_C5, n7);
+ delay(n7);
+
+ tone(8,  NOTE_C5, n3);
+ delay(n3);
+
+  tone(8,  NOTE_A5, n3);
+ delay(n3);
+
+tone(8,  NOTE_A5, n4);
+ delay(n4);
+
+ tone(8,  NOTE_A5, n1);
+ delay(n1);
+
+ tone(8,  NOTE_B5, n1);
+ delay(n1);
+
+tone(8,  NOTE_C6, n1);
+ delay(n1);
+
+ tone(8,  NOTE_B5, n1);
+ delay(n1);
+
+ tone(8,  NOTE_A5, n1);
+ delay(n1);
+
+ tone(8,  NOTE_G5, n3);
+ delay(n3);
+
+ tone(8,  NOTE_G5, n7);
+ delay(n7);
+
+  tone(8,  NOTE_F5, n2);
+ delay(n2);
+
+  tone(8,  NOTE_E5, n1);
+ delay(n1);
+
+  tone(8,  NOTE_F5, n3);
+ delay(n3);
+
+  tone(8,  NOTE_F5, n5);
+ delay(n5);
+
+  tone(8,  NOTE_F5, n1);
+ delay(n1);
+
+  tone(8,  NOTE_G5, n1);
+ delay(n1);
+
+ tone(8,  NOTE_A5, n1);
+ delay(n1);
+
+ tone(8,  NOTE_G5, n1);
+ delay(n1);
+
+ tone(8,  NOTE_F5, n1);
+ delay(n1);
+
+ tone(8,  NOTE_E5, n3);
+ delay(n3);
+
+  tone(8,  NOTE_E5, n7);
+ delay(n7);
+
+  tone(8,  NOTE_E5, n2);
+ delay(n2);
+
+  tone(8,  NOTE_E5, n1);
+ delay(n1);
+
+  tone(8,  NOTE_A5, n3);
+ delay(n3);
+
+  tone(8,  NOTE_A5, n5);
+ delay(n5);
+
+  tone(8,  NOTE_A5, n1);
+ delay(n1);
+
+  tone(8,  NOTE_B5, n1);
+ delay(n1);
+
+  tone(8,  NOTE_C6, n1);
+ delay(n1);
+
+  tone(8,  NOTE_B5, n1);
+ delay(n1);
+
+  tone(8,  NOTE_A5, n1);
+ delay(n1);
+
+ tone(8,  NOTE_G5, n3);
+ delay(n3);
+
+ tone(8,  NOTE_G5, n7);
+ delay(n7);
+
+ tone(8,  NOTE_F5, n2);
+ delay(n2);
+
+  tone(8,  NOTE_E5, n1);
+ delay(n1);
+
+ tone(8,  NOTE_F5, n3);
+ delay(n3);
+
+ tone(8,  NOTE_F5, n5);
+ delay(n5);
+
+  tone(8,  NOTE_D5, n1);
+ delay(n1);
+
+  tone(8,  NOTE_E5, n1);
+ delay(n1);
+
+  tone(8,  NOTE_F5, n1);
+ delay(n1);
+
+  tone(8,  NOTE_E5, n1);
+ delay(n1);
+
+  tone(8,  NOTE_D5, n1);
+ delay(n1);
+
+  tone(8,  NOTE_C5, n6);
+ delay(n6);
+ 
+
+ 
+}
+ 
+void loop() {
+
+}
